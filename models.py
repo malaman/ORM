7098f1563dcc5adfd5a3519652f8fbeaa4a3805f
unkrunner <unk.runner@yandex.ua>
2014-06-04 10:34:46 +0300

    first commit

---
+++ models.py
@@ -0,0 +1,46 @@
+from entity import *
+
+class Section(Entity):
+    _columns  = ['title']
+    _parents  = []
+    _children = {'categories': 'Category'}
+    _siblings = {}
+
+class Category(Entity):
+    _columns  = ['title']
+    _parents  = ['section']
+    _children = {'posts': 'Post'}
+    _siblings = {}
+
+class Post(Entity):
+    _columns  = ['content', 'title']
+    _parents  = ['category']
+    _children = {'comments': 'Comment'}
+    _siblings = {'tags': 'Tag'}
+
+class Comment(Entity):
+    _columns  = ['text']
+    _parents  = ['post', 'user']
+    _children = {}
+    _siblings = {}
+
+class Tag(Entity):
+    _columns  = ['name']
+    _parents  = []
+    _children = {}
+    _siblings = {'posts': 'Post'}
+
+class User(Entity):
+    _columns  = ['name', 'email', 'age']
+    _parents  = []
+    _children = {'comments': 'Comment'}
+    _siblings = {}
+
+
+if __name__ == "__main__":
+    section = Section()
+    section.title = "zalupa"
+    section.save()
+
+    for section in Section.all():
+        print(section.title)
